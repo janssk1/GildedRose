f678087325dd845a22f89076179fc42bd705d1fa
janssk1 <koen.janssens@nokia.com>
2020-07-20 16:03:05 +0200

    * Added Conjured
    Change-Id: Ifba5869ac9639c11d34b1cc1626e9654862d646e

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,103 +1,65 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+    interface QualityFunction {
+        /**
+         * @return updated quality, taking into account remaining days and current quality
+         */
+        int getUpdatedQuality(int remainingDays, int currentQuality);
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-        System.out.println("OMGHAI!");
-		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+    private static final QualityFunction BRIE = (remainingDays, quality) -> {
+        quality++;
+        if (remainingDays < 0) {
+            quality++;//todo: this behavior is not documented !
+        }
+        return quality;
+    };
 
-        updateQuality();
-}
+    private static final QualityFunction BACKSTAGE = (remainingDays, quality) -> {
+        if (remainingDays < 0) {
+            return 0;
+        } else if (remainingDays < 5) {
+            return quality + 3;
+        } else if (remainingDays < 10) {
+            return quality + 2;
+        } else {
+            return quality + 1;
+        }
+    };
 
+    private final List<Item> items = new LinkedList<>();
 
-	
-    public static void updateQuality()
-    {
-        for (int i = 0; i < items.size(); i++)
-        {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
+    public void addItem(Item item) {
+        items.add(item);
+    }
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
+    private static int getDefaultQualityDegrade(int remaining) {
+        return remaining < 0 ? 2 : 1;
+    }
 
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
+    private QualityFunction getQualityUpdater(String name) {
+        if (name.equals("Aged Brie")) {
+            return BRIE;
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return BACKSTAGE;
+        } else if (name.contains("Conjured")) {
+            return (remaining, quality) -> quality - 2 * getDefaultQualityDegrade(remaining);
+        } else {
+            return (remaining, quality) -> quality - getDefaultQualityDegrade(remaining);
+        }
+    }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
-
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
+    public void updateQuality() {
+        for (Item item : items) {
+            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                item.sellIn -= 1;
+                int newQuality = getQualityUpdater(item.name).getUpdatedQuality(item.sellIn, item.quality);
+                item.quality = Math.min(newQuality, 50);
+                item.quality = Math.max(0, item.quality);
             }
         }
     }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,59 @@
-import static org.junit.Assert.*;
-
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertThat;
 
 
+@RunWith(Parameterized.class)
 public class GildedRoseTest {
+
+	private final GildedRose unit = new GildedRose();
+	private final Item item;
+	private final int nextSellIn;
+	private final int nextQuality;
+
+	public GildedRoseTest(String item, int sellIn, int quality, int nextSellIn, int nextQuality) {
+		this.nextSellIn = nextSellIn;
+		this.nextQuality = nextQuality;
+
+		this.item = new Item(item, sellIn, quality);
+		unit.addItem(this.item);
+	}
+
+	@Parameterized.Parameters(name = "{0} {1}/{2}->{3}/{4}")
+	public static Object[][] params() {
+		return new Object[][] {
+				new Object[] {"+5 Dexterity Vest", 10, 20, 9, 19},
+				new Object[] {"+5 Dexterity Vest", 10, 0, 9, 0},
+				new Object[] {"+5 Dexterity Vest", 0, 0, -1, 0},
+				new Object[] {"+5 Dexterity Vest", -1, 10, -2, 8},
+
+				new Object[] {"Aged Brie", 10, 10, 9, 11},
+				new Object[] {"Aged Brie", 10, 50, 9, 50},
+				new Object[] {"Aged Brie", 0, 1, -1, 3},//this seems a bug to me... Quality should be 2
+
+				new Object[] {"Sulfuras, Hand of Ragnaros", 0, 80, 0, 80},
+
+				new Object[] {"Backstage passes to a TAFKAL80ETC concert", 11, 10, 10, 11},
+				new Object[] {"Backstage passes to a TAFKAL80ETC concert", 10, 10, 9, 12},
+				new Object[] {"Backstage passes to a TAFKAL80ETC concert", 4, 10, 3, 13},
+				new Object[] {"Backstage passes to a TAFKAL80ETC concert", 1, 10, 0, 13},
+				new Object[] {"Backstage passes to a TAFKAL80ETC concert", 0, 50, -1, 0},
+
+				//
+				new Object[] {"Conjured Mana Cake", 10, 10, 9, 8},
+				new Object[] {"Conjured Mana Cake", 0, 10, -1, 6},
+				new Object[] {"Conjured Mana Cake", 0, 2, -1, 0},
+		};
+	}
+
 
 	@Test
 	public void testTheTruth() {
-		assertTrue(true);
+		unit.updateQuality();
+		assertThat("unexpected sellIn", item.getSellIn(), CoreMatchers.equalTo(nextSellIn));
+		assertThat("unexpected quality", item.getQuality(), CoreMatchers.equalTo(nextQuality));
 	}
 }
